efed85c2decb214d714be0380394b3988c8e2038
tymiurge <tmrlabas@gmail.com>
2018-03-21 21:47:11 +0200

    express-mongodb-crud boilderplate

---
+++ express-mongodb-crud/README.md
@@ -0,0 +1,7 @@
+#### based on
+https://ciphertrick.com/2017/10/23/dockerize-nodejs-service-with-mongodb-docker-compose/
+
+#### to connect to mongo db from studio 3t
+![connection example](docs/pic1.png)
+
+
---
+++ express-mongodb-crud/models/user.model.js
@@ -0,0 +1,13 @@
+module.exports = function(mongoose){
+    return [
+    {
+        name: {type: String, required: true},
+        email: {type: String}
+    }, {
+        timestamps: true,
+        createdby: true,
+        updatedby: true
+    }
+    ]
+};
+
